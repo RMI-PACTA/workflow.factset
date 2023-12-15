dc570c3a3a695a0b8296e6f3e7256ebe9fab5bc9
Alex Axthelm <aaxthelm@RMI.org>
2023-12-15 15:37:36 +0100

    Convert get_factset_isin_to_fund_table to package

--- R/get_factset_isin_to_fund_table.R
+++ R/get_factset_isin_to_fund_table.R
@@ -1,47 +1,40 @@
 #' Get the isin_to_fund_table data from the FactSet database and prepare the
 #' `factset_isin_to_fund_table` tibble
 #'
-#' @param ... Arguments to be passed to the `connect_factset_db()` function (for
-#'   specifying database connection parameters)
+#' @param conn database connection
 #'
 #' @return A tibble properly prepared to be saved as the
 #'   `factset_isin_to_fund_table.rds` output file
 #'
 #' @export
 
-get_factset_isin_to_fund_table <-
-  function(...) {
-    # connect to the FactSet database ------------------------------------------
-    factset_db <- connect_factset_db(...)
+get_factset_isin_to_fund_table <- function(conn) {
+  # get the ISIN to fsym_id table --------------------------------------------
+
+  logger::info("Getting ISIN to fsym_id mapping")
+  isin <-
+    dplyr::tbl(conn, "sym_v1_sym_isin") %>%
+    dplyr::select("isin", "fsym_id")
 
 
-    # get the ISIN to fsym_id table --------------------------------------------
+  # get the fsym_id to fund_id table -----------------------------------------
 
-    isin__fsym_id <-
-      tbl(factset_db, "sym_v1_sym_isin") %>%
-      select("isin", "fsym_id")
-
-
-    # get the fsym_id to fund_id table -----------------------------------------
-
-    fsym_id__factset_fund_id <-
-      tbl(factset_db, "own_v5_own_ent_fund_identifiers") %>%
-      dplyr::filter(.data$identifier_type == "FSYM_ID") %>%
-      select(fsym_id = "fund_identifier", "factset_fund_id")
+  logger::info("Getting fsym_id to fund id mapping")
+  fund_id <-
+    dplyr::tbl(conn, "own_v5_own_ent_fund_identifiers") %>%
+    dplyr::filter(.data$identifier_type == "FSYM_ID") %>%
+    dplyr::select(fsym_id = "fund_identifier", "factset_fund_id")
 
 
-    # merge and collect the data, then disconnect ------------------------------
+  # merge and collect the data ------------------------------
 
-    isin__factset_fund_id <-
-      fsym_id__factset_fund_id %>%
-      inner_join(isin__fsym_id, by = "fsym_id") %>%
-      select("isin", "fsym_id", "factset_fund_id") %>%
-      dplyr::collect()
+  logger::info("Merging ISIN to fsym_id and fsym_id to fund_id")
+  isin__factset_fund_id <-
+    fund_id %>%
+    dplyr::inner_join(isin, by = "fsym_id") %>%
+    dplyr::select("isin", "fsym_id", "factset_fund_id") %>%
+    dplyr::collect()
 
-    DBI::dbDisconnect(factset_db)
-
-
-    # return the ISIN to fund_id table -----------------------------------------
-
-    isin__factset_fund_id
-  }
+  # return the ISIN to fund_id table -----------------------------------------
+  return(isin__factset_fund_id)
+}
