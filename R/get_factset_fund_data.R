d7d5ef2b58d4e12696c5841a4721c915c42c971b
Alex Axthelm <aaxthelm@RMI.org>
2023-12-18 16:34:54 +0100

    Update fund data function

--- R/get_factset_fund_data.R
+++ R/get_factset_fund_data.R
@@ -1,84 +1,98 @@
 #' Get the fund data from the FactSet database and prepare the
 #' `factset_fund_data` tibble
 #'
+#' @param conn databse connection
 #' @param data_timestamp A single string specifying the desired date for the
 #'   data in the form "2021-12-31"
-#' @param ... Arguments to be passed to the `connect_factset_db()` function (for
-#'   specifying database connection parameters)
 #'
 #' @return A tibble properly prepared to be saved as the `factset_fund_data.rds`
 #'   output file
 #'
 #' @export
 
-get_factset_fund_data <-
-  function(data_timestamp, ...) {
-    # connect to the FactSet database ------------------------------------------
-    factset_db <- connect_factset_db(...)
+get_factset_fund_data <- function(conn, data_timestamp) {
+  # get the fund holdings and the holdings' reported market value ------------
+
+  logger::log_debug("Extracting financial info from database.")
+  logger::log_info("using data timestamp: ", data_timestamp)
+
+  logger::log_trace(
+    "Accessing historical fund holdings - security level. ",
+    "Filtering to date: {data_timestamp}"
+  )
+  fund_security <-
+    dplyr::tbl(conn, "own_v5_own_fund_detail") %>%
+    dplyr::filter(.data$report_date == .env$data_timestamp) %>%
+    dplyr::select(
+      factset_fund_id = "factset_fund_id",
+      holding_fsym_id = "fsym_id",
+      holding_reported_mv = "reported_mv"
+    )
+
+  logger::log_trace(
+    "Accessing historical fund holdings - non-securities. ",
+    "Filtering to date: {data_timestamp}"
+  )
+  fund_nonsecurity <-
+    dplyr::tbl(conn, "own_v5_own_fund_generic") %>%
+    dplyr::filter(.data$report_date == .env$data_timestamp) %>%
+    dplyr::select(
+      factset_fund_id = "factset_fund_id",
+      holding_fsym_id = "generic_id",
+      holding_reported_mv = "reported_mv"
+    )
+
+  logger::log_trace(
+    "Combining historical fund holdings - security and non-security."
+  )
+  fund_holding <-
+    dplyr::union_all(
+      fund_security,
+      fund_nonsecurity
+    )
 
 
-    # get the fund holdings and the holdings' reported market value ------------
+  # get the fund total reported market value ---------------------------------
 
-    factset_fund_id__holding_fsym_id <-
-      tbl(factset_db, "own_v5_own_fund_detail") %>%
-      dplyr::filter(.data$report_date == .env$data_timestamp) %>%
-      select(
-        factset_fund_id = "factset_fund_id",
-        holding_fsym_id = "fsym_id",
-        holding_reported_mv = "reported_mv"
-      )
-
-
-    # --------------------------------------------------------------------------
-
-    factset_fund_id__generic_id <-
-      tbl(factset_db, "own_v5_own_fund_generic") %>%
-      dplyr::filter(.data$report_date == .env$data_timestamp) %>%
-      select(
-        factset_fund_id = "factset_fund_id",
-        holding_fsym_id = "generic_id",
-        holding_reported_mv = "reported_mv"
-      )
-
-    factset_fund_id__holding_fsym_id <-
-      dplyr::union_all(
-        factset_fund_id__holding_fsym_id,
-        factset_fund_id__generic_id
-      )
+  logger::log_trace(
+    "Accessing historical fund filings.",
+    "Filtering to date: {data_timestamp}"
+  )
+  fund_mv <-
+    dplyr::tbl(conn, "own_v5_own_ent_fund_filing_hist") %>%
+    dplyr::filter(.data$report_date == .env$data_timestamp) %>%
+    dplyr::select("factset_fund_id", "total_reported_mv")
 
 
-    # get the fund total reported market value ---------------------------------
-
-    factset_fund_id__total_reported_mv <-
-      tbl(factset_db, "own_v5_own_ent_fund_filing_hist") %>%
-      dplyr::filter(.data$report_date == .env$data_timestamp) %>%
-      select("factset_fund_id", "total_reported_mv")
-
-
-    # symbology containing the ISIN to fsym_id link
-    fsym_id__isin <-
-      tbl(factset_db, "sym_v1_sym_isin")
+  logger::log_trace(
+    "Accessing current ISIN mappings.",
+  )
+  # symbology containing the ISIN to fsym_id link
+  fsym_id__isin <-
+    dplyr::tbl(conn, "sym_v1_sym_isin")
 
 
-    # merge and collect the data, then disconnect ------------------------------
+  # merge and collect the data, then disconnect ------------------------------
 
-    fund_data <-
-      factset_fund_id__total_reported_mv %>%
-      filter(.data$total_reported_mv != 0 | !is.na(.data$total_reported_mv)) %>%
-      left_join(factset_fund_id__holding_fsym_id, by = "factset_fund_id") %>%
-      left_join(fsym_id__isin, by = c(`holding_fsym_id` = "fsym_id")) %>%
-      select(
-        factset_fund_id = "factset_fund_id",
-        fund_reported_mv = "total_reported_mv",
-        holding_isin = "isin",
-        holding_reported_mv = "holding_reported_mv"
-      ) %>%
-      dplyr::collect()
+  logger::log_trace("Merging the data.")
+  fund_data <-
+    fund_mv %>%
+    dplyr::filter(
+      .data$total_reported_mv != 0 | !is.na(.data$total_reported_mv)
+    ) %>%
+    dplyr::left_join(fund_holding, by = "factset_fund_id") %>%
+    dplyr::left_join(fsym_id__isin, by = c(`holding_fsym_id` = "fsym_id")) %>%
+    dplyr::select(
+      factset_fund_id = "factset_fund_id",
+      fund_reported_mv = "total_reported_mv",
+      holding_isin = "isin",
+      holding_reported_mv = "holding_reported_mv"
+    )
 
-    DBI::dbDisconnect(factset_db)
+  logger::log_trace("Downloading fund data.")
+  fund_data <- dplyr::collect(fund_data)
 
+  # return the fund data -----------------------------------------------------
 
-    # return the fund data -----------------------------------------------------
-
-    fund_data
-  }
+  return(fund_data)
+}
