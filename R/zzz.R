2e487687002272fc728b07063c8dc3f7f54d91a1
Alex Axthelm <aaxthelm@RMI.org>
2024-03-12 13:58:44 +0100

    Add namespaced dbplyr function to suppress NOTE

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onLoad <- function(lib, pkg) {
+  # Work around bug in code checking in R 4.2.2 for use of packages
+  # Suppresses NOTE in R CMD CHECK
+  # See https://stackoverflow.com/a/75384338
+  dummy <- function() dbplyr::is.sql
+}
